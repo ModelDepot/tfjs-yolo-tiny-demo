b5fb1ea727745335c241a445e87532358eb3e25c
Mike Shi <michaelshi42@gmail.com>
2018-04-08 12:20:18 -0700

    Init Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# In-Browser ML-Powered Object Detection Demo
+
+Use YOLO deep learning model to detect objects locally in your browser
+(no servers required).
+
+Want to use object detection in your project? Check out [the NPM package here](https://www.npmjs.com/package/tfjs-yolo-tiny).
+
+## Demo
+
+(You can only get so far with 1 FPS)
+
+![yolo person detection](https://github.com/ModelDepot/tfjs-yolo-tiny/raw/master/assets/demo.gif)
+
+LIVE DEMO WILL BE OUT SOON!
+
+# Get Started
+
+## Install Dependencies
+    yarn
+
+## Serve Demo
+    yarn serve
+
+# Performance
+
+I get around 1 FPS on my Macbook 13" mid-2014 using Chrome. Your mileage may vary. PRs to help perf/bug fixes are certainly welcome at [tfjs-yolo-tiny](https://github.com/ModelDepot/tfjs-yolo-tiny).
---
+++ src/index.js
@@ -0,0 +1,61 @@
+import * as tf from '@tensorflow/tfjs';
+import yolo, { downloadModel } from 'tfjs-yolo-tiny';
+
+import { Webcam } from './webcam';
+
+let model;
+const webcam = new Webcam(document.getElementById('webcam'));
+
+(async function main() {
+  await webcam.setup();
+  model = await downloadModel();
+
+  run();
+})();
+
+async function run() {
+  clearRects();
+
+  const inputImage = webcam.capture();
+
+  const t0 = performance.now();
+
+  const boxes = await yolo(inputImage, model);
+
+  const t1 = performance.now();
+  console.log("YOLO inference took " + (t1 - t0) + " milliseconds.");
+
+  boxes.forEach(box => {
+    const {
+      top, left, bottom, right, classProb, className,
+    } = box;
+
+    drawRect(left, top, right-left, bottom-top, `${className} ${classProb}`)
+  });
+
+  await tf.nextFrame();
+
+  run();
+}
+
+const webcamElem = document.getElementById('webcam-wrapper');
+
+export function drawRect(x, y, w, h, text = '', color = 'red') {
+  const rect = document.createElement('div');
+  rect.classList.add('rect');
+  rect.style.cssText = `top: ${y}; left: ${x}; width: ${w}; height: ${h}; border-color: ${color}`;
+
+  const label = document.createElement('div');
+  label.classList.add('label');
+  label.innerText = text;
+  rect.appendChild(label);
+
+  webcamElem.appendChild(rect);
+}
+
+export function clearRects() {
+  const rects = document.getElementsByClassName('rect');
+  while(rects[0]) {
+    rects[0].parentNode.removeChild(rects[0]);
+  }
+}
